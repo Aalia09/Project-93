3c611a54bf58e573db8729793debe5404a0bbe6e
Aalia09 <76902204+Aalia09@users.noreply.github.com>
2021-05-08 15:49:24 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBCUGt7KFdIUP-Kgya8VpZ3B4h9uP07goY",
+    authDomain: "kwitter-d58b7.firebaseapp.com",
+    projectId: "kwitter-d58b7",
+    storageBucket: "kwitter-d58b7.appspot.com",
+    messagingSenderId: "426386250237",
+    appId: "1:426386250237:web:eaf95c695cb5dd177d6194"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
